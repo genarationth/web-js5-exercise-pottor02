21fb5a4532683144ef7895b6aef4e993fd3feb0e
pottor02 <doramon_toon_5@hotmail.com>
2023-03-16 01:47:34 +0700

    Finnish01

---
+++ Exercise01.js
@@ -0,0 +1,14 @@
+
+const people = ['Sofia','David','Juan'];
+
+console.log('Part1 = ' + people);
+
+people.push('Sara','Augustin')
+
+console.log('Part2 = ' + people);
+
+people.splice(3,0,'Reneta');
+
+people.push('Elena')
+
+console.log('Part3 = ' + people);
---
+++ Exercise02.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 5; i++) {
+    let star = '';
+    for (let j = 0; j < i + 1; j++) {
+        stars += '* '
+    }
+    console.log(stars);
+
+}
+
+let stars = [];
