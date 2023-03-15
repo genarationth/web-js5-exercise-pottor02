fba3498daa5ac713b0ff7627ec5703be9b7d9086
pottor02 <doramon_toon_5@hotmail.com>
2023-03-16 01:49:44 +0700

    Finnish01

--- Exercise02.js
+++ Exercise02.js
@@ -1,10 +1,10 @@
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i <= 5; i++) {
     let star = '';
-    for (let j = 0; j < i + 1; j++) {
+    for (let j = 0; j <= i + 1; j++) {
         stars += '* '
     }
-    console.log(stars);
+    console.log(star);
 
 }
 
-let stars = [];
+let star = [];
